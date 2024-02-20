3b862254b5854994465b595f2c5e5cd25f21813e
Gubrud, Aaron D <aaron.d.gubrud@intel.com>
2024-02-20 07:31:51 -0800

    adding init.h

---
+++ csrc/cpu/tpp/init.h
@@ -0,0 +1,26 @@
+/******************************************************************************
+ * Copyright (c) 2022 Intel Corporation - All rights reserved.                *
+ *                                                                            *
+ * For information on the license, see the LICENSE file.                      *
+ * Further information: https://github.com/libxsmm/tpp-pytorch-extension/     *
+ * SPDX-License-Identifier: BSD-3-Clause                                      *
+ ******************************************************************************/
+/* Author: Dhiraj Kalamkar (Intel Corp.)
+ ******************************************************************************/
+
+//#include <ATen/record_function.h>
+//#include <torch/csrc/autograd/VariableTypeUtils.h>
+#include <torch/extension.h>
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+namespace torch_ipex {
+namespace tpp {
+
+void reset_debug_timers();
+void print_debug_timers(int tid, bool detailed);
+void print_debug_thread_imbalance();
+}
+}
